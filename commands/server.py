dd54252ae3192b010565d3bc7e6d4fed8bb8fb75
M336 <75208577+M336G@users.noreply.github.com>
2024-04-26 18:14:10 +0200

    fix?

--- commands/server.py
+++ commands/server.py
@@ -13,13 +13,13 @@ def commandFunction(tree, client):
             server = data["server"]
             lang = get_language(interaction.user.id)
             if interaction.user.id != 629711559899217950:
-                embed = Embed(title=" ",description=f"**:x: {translate("error.no_permission", lang)}**",colour=15548997)
+                embed = Embed(title=" ",description=f"**:x: {format(translate("error.no_permission", lang))}**",colour=15548997)
                 await interaction.response.send_message(" ",embed=embed, ephemeral=True)
                 log(f"(FAILED) {interaction.user} FAILED to change the server (not allowed)")
                 return
         
             if interaction.guild.id == server:
-                embed = Embed(title=" ",description=f"**:x: {translate("cmd.error.server.same", lang)}**",colour=15548997)
+                embed = Embed(title=" ",description=f"**:x: {format(translate("cmd.error.server.same", lang))}**",colour=15548997)
                 await interaction.response.send_message(" ",embed=embed, ephemeral=True)
                 log(f"(FAILED) {interaction.user} FAILED to change the server (same server)")
                 return
@@ -30,10 +30,10 @@ def commandFunction(tree, client):
                 try:
                     with open("specialConfig.json", "w") as specialConfigFile:
                         dump(data, specialConfigFile, indent=4)
-                    embed = Embed(title=" ",description=f"**:white_check_mark: {translate("cmd.server.success", lang)}** ``{interaction.guild}``!",colour=2067276)
+                    embed = Embed(title=" ",description=f"**:white_check_mark: {format(translate("cmd.server.success", lang))}** ``{interaction.guild}``!",colour=2067276)
                     await interaction.response.send_message(" ",embed=embed)
                     log(f"(SUCCESS) Server has been CHANGED")
                 except:
-                    embed = Embed(title=" ",description=f"**:x: {translate("error.generic", lang)}**",colour=15548997)
+                    embed = Embed(title=" ",description=f"**:x: {format(translate("error.generic", lang))}**",colour=15548997)
                     await interaction.response.send_message(" ",embed=embed, ephemeral=True)
                     log(f"(FAILED) {interaction.user} FAILED to change the server (error when writing in specialConfig.json/when sending the success embed)")
