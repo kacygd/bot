3cac688d626a9f0aad3ee99a4803a9f8c8d8893d
M336 <75208577+M336G@users.noreply.github.com>
2024-06-24 18:20:44 +0200

    Better main.py

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ from discord.ext import tasks
 from datetime import datetime, timedelta
 from json import dump, load
 
+firstCon:bool = False
+async def first_connection(self):
+    global firstCon
+    if firstCon == False:
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
+        print(f"Logged in as {self.user}.")
+        await client.change_presence(activity=Activity(type=ActivityType.playing, name="PlusGDPS"))
+        log(f"(SUCCESS) {self.user} has been STARTED. Ping: {round (client.latency * 1000)} ms")
+        firstCon = True
+
 class aclient(Client):
     def __init__(self):
         super().__init__(intents=Intents.default())
@@ -16,21 +28,16 @@ class aclient(Client):
         self.context_menus = {}
     
     async def on_ready(self):
-        if not self.synced:
-            await tree.sync()
-            self.synced = True
-        print(f"Logged in as {self.user}.")
-        await client.change_presence(activity=Activity(type=ActivityType.playing, name="PlusGDPS"))
-        log(f"(SUCCESS) {self.user} has been STARTED. Ping: {round (client.latency * 1000)} ms")
+        await first_connection(self)
         await thumbnails_delete_and_poll_lock.start()
-        
+
 client = aclient()
 tree = CommandTree(client)
 
 for command in listdir("commands"):
     if (not(isfile(join("commands", command)))):
         continue
-
+            
     module = __import__("commands." + command[:-3], fromlist=["commandFunction"])
     commandObject = client.commands[command] = globals()[module.__name__] = module
     commandObject.commandFunction(tree, client)
@@ -39,7 +46,7 @@ for command in listdir("context_menu"):
     if (not(isfile(join("context_menu", command)))):
         continue
             
-    module = __import__("context_menu." + command[:-3], fromlist=["commandFunction"])  # Remove the .py extension
+    module = __import__("context_menu." + command[:-3], fromlist=["commandFunction"])
     commandObject = client.context_menus[command] = globals()[module.__name__] = module
     commandObject.commandFunction(tree, client)
 
@@ -94,5 +101,5 @@ async def bg_task():
                 dump(json_dict, thumbnailsFile, indent=4)
 
             log(f"(CLEANUP) DELETED thumbnail '{base}'")
-            
+
 client.run(TOKEN)
